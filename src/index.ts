081debd93c2c4468a40769020afd2d30f15c0412
ibraheem saoud <saoudibraheem@gmail.com>
2020-11-06 12:30:15 +0100

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import './style.css';
-import printMe from './print';
-
-
 function component() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
 
   element.innerHTML = "hello";
   element.classList.add('hello');
   
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
   return element;
 }
 
